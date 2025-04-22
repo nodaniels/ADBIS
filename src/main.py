b83e38a0a79a9f1462fc96125c782f8e43a666f2
nodaniels <noahnfdn@gmail.com>
2025-04-22 12:27:04 +0200

    projektet

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# Main function to run the Streamlit app
+def main():
+    st.title("Afdelingskommunikation App")
+    st.sidebar.title("Navigation")
+
+
+
+if __name__ == "__main__":
+    main()
